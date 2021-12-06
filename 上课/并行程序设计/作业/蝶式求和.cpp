926f8f4f3c965dc09bb9a8f958f3c27d7f708d2f
MacOs ghh <guanhuhao@gmail.com>
2021-12-06 15:32:28 +0800

    2021.12.03

--- 上课/并行程序设计/作业/蝶式求和.cpp
+++ 上课/并行程序设计/作业/蝶式求和.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 
-    for(int i = 1; i <= numprocs ; i<<=1){
+    for(int i = 1; i < numprocs ; i<<=1){
         MPI_Send(data,1,MPI_INT,myid^i,myid^i,MPI_COMM_WORLD);
         MPI_Recv(data+1,1,MPI_INT,myid^i,myid,MPI_COMM_WORLD,&status);
         data[0] += data[1];
