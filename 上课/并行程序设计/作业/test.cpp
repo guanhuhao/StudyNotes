f1de452882198aa45bbe49985a222cfc84146bf3
3060 <1057368050@qq.com>
2022-01-06 23:22:30 +0800

    QR test

--- 上课/并行程序设计/作业/test.cpp
+++ 上课/并行程序设计/作业/test.cpp
@@ -1,211 +1,114 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include<bits/stdc++.h>
-#include "mpi.h"
-#define GHH(...) printf(__VA_ARGS__)
-#define a(x,y) a[x*M+y]
-/*A为M*M矩阵*/
-#define A(x,y) A[x*M+y]
-#define l(x,y) l[x*M+y]
-#define u(x,y) u[x*M+y]
-#define floatsize sizeof(float)
-#define intsize sizeof(int)
- 
-int M,N;
-int m;
-float *A;
-int my_rank;
-int p;
-MPI_Status status;
- 
-void fatal(char *message)
-{
-    printf("%s\n",message);
-    exit(1);
-}
- 
-void Environment_Finalize(float *a,float *f)
-{
-    free(a);
-    free(f);
-}
- 
-int main(int argc, char **argv)
-{
-    int i,j,k,my_rank,group_size;
-    int i1,i2;
-    int v,w;
-    float *a,*f,*l,*u;
-    FILE *fdA;
- 
-    MPI_Init(&argc,&argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&group_size);
-    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
- 
-    p=group_size;
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-    if (my_rank==0)
-    {
-        // fdA=fopen("dataIn.txt","r");
-        M = N = 1000;
-        // fscanf(fdA,"%d %d", &M, &N);
-        // if(M != N)
-        // {
-        //     puts("The input is error!");
-        //     exit(0);
-        // }
-        A=(float *)malloc(floatsize*M*M);
-        for(i = 0; i < M; i ++)
-            for(j = 0; j < M; j ++)
-                A[i*M+j] = rand()%100;
-        //         fscanf(fdA, "%f", A+i*M+j);
-        // fclose(fdA);
-    }
- 
-    clock_t begin = clock();
+#define A(x, y) A[x * N + y]
+#define Q(x, y) Q[x * N + y]
 
-    /*0号进程将M广播给所有进程*/
-    MPI_Bcast(&M,1,MPI_INT,0,MPI_COMM_WORLD);
-    m=M/p;
-    if (M%p!=0) m++;	//M/p向上取余 
- 
-    /*分配至各进程的【子矩阵大小为m*M】*/
-    a=(float*)malloc(floatsize*m*M);
- 
-    /*各进程为主行元素建立【发送和接收缓冲区】*/
-    f=(float*)malloc(floatsize*M);
- 
-    /*0号进程为l和u矩阵分配内存，以分离出经过变换后的A矩阵中的l和u矩阵*/
-    if (my_rank==0)
+int N;
+float *A, *Q;
+
+void  wait(int line, int base, int *condition)
+{
+    while (1)
     {
-        l=(float*)malloc(floatsize*M*M);
-        u=(float*)malloc(floatsize*M*M);
-    }
- 
-    /*0号进程采用行交叉划分将矩阵A划分为大小【m*M的p块子矩阵】，依次发送给1至p-1号进程*/
-    if (a==NULL) fatal("allocate error\n");
-    if (my_rank==0)
-    {
-        for(i=0;i<m;i++)
-            for(j=0;j<M;j++)
-                a(i,j)=A(i,j);	//0号进程自己的子矩阵
-				 
-        for(i=1;i<p;i++)
+        if (condition[line] >= base)
         {
-        	for(j=i*m;j<(i+1)*m;j++){
-        		if(j<M){
-            		i1=j%m;
-            		MPI_Send(&A(j,0),M,MPI_FLOAT,i,i1,MPI_COMM_WORLD);//发送给i1，tag是i2 
-            	}
-        	}
+            break;
         }
     }
-    else
+}
+void transform(int j, int start, int end)
+{
+    for (int i = start; i < end; ++i)
     {
-        for(i=0;i<m;i++)
-            MPI_Recv(&a(i,0),M,MPI_FLOAT,0,i,MPI_COMM_WORLD,&status);
-    }
-    for(i=0;i<m;i++)
-        for(j=0;j<p;j++)
-    {
-        /*j号进程负责广播主行元素*/
-        if (my_rank==j)
+        float sq = sqrt(A(j, j) * A(j, j) + A(i, j) * A(i, j));
+        float c = A(j, j) / sq;
+        float s = A(i, j) / sq;
+        float ajk, aik, qjk, qik;
+        for (int k = 0; k < N; ++k)
         {
-            v=i*p+j;
-            for (k=v;k<M;k++)
-                f[k]=a(i,k);
- 
-            MPI_Bcast(f,M,MPI_FLOAT,my_rank,MPI_COMM_WORLD);
+            ajk = c * A(j, k) + s * A(i, k);
+            qjk = c * Q(j, k) + s * Q(i, k);
+            aik = -s * A(j, k) + c * A(i, k);
+            qik = -s * Q(j, k) + c * Q(i, k);
+            A(j, k) = ajk;
+            Q(j, k) = qjk;
+            A(i, k) = aik;
+            Q(i, k) = qik;
+        }
+    }
+}
+
+void randData(){
+    freopen("data.txt","w",stdout);
+    int n=1000;
+    printf("%d\n",n);
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            printf("%d ",rand()%100);
+
+    fclose(stdout);
+}
+
+int main(int argc, char **argv)
+{
+    double start, end;
+    int threads_num = 16;
+    FILE *fp = fopen("data.txt", "r");
+    fscanf(fp, "%d", &N);
+    A = (float *)malloc(N * N * sizeof(float));
+    for (int i = 0; i < N * N; ++i)
+    {
+        fscanf(fp, "%f", A + i);
+    }
+    fclose(fp);
+    Q = (float *)malloc(N * N * sizeof(float));
+    for (int i = 0; i < N * N; ++i)
+    {
+        if (i / N == i % N)
+        {
+            Q[i] = 1;
         }
         else
         {
-            v=i*p+j;
-            MPI_Bcast(f,M,MPI_FLOAT,j,MPI_COMM_WORLD);
+            Q[i] = 0;
         }
- 
-        /*编号小于my_rank的进程（包括my_rank本身）利用主行对其第i+1,…,m-1行数据做行变换*/
-        if (my_rank<=j)
-            for(k=i+1;k<m;k++)
+    }
+    int *condition = (int *)malloc(N * sizeof(int));
+    int total_thread, my_thread, block, base;
+    for (int i = 0; i < N; i++)
+    {
+        condition[i] = 0;
+    }
+    start = omp_get_wtime();
+    omp_set_num_threads(threads_num);
+#pragma omp parallel shared(Q, A, condition) private(my_thread, total_thread, base, block)
+    {
+        my_thread = omp_get_thread_num();
+        total_thread = omp_get_num_threads();
+        block = N / total_thread;
+        base = my_thread * block;
+        for (int j = 0; j < base + block; ++j)
         {
-            a(k,v)=a(k,v)/f[v];
-            // #pragma omp parallel for private(k,w,v) shared(f,a)
-            for(w=v+1;w<M;w++)
-                a(k,w)=a(k,w)-f[w]*a(k,v);
-        }
- 
-        /*编号大于my_rank的进程利用主行对其第i,…,m-1行数据做行变换*/
-        if (my_rank>j)
-            for(k=i;k<m;k++)
-        {
-            a(k,v)=a(k,v)/f[v];
-            // #pragma omp parallel for private(k,w,v) shared(a,f,M)
-            for(w=v+1;w<M;w++)
-                a(k,w)=a(k,w)-f[w]*a(k,v);
+            if (j < base)
+            {
+                 wait(j, base, condition);
+                transform(j, base, base + block);
+            }
+            else
+            {
+                transform(j, j + 1, base + block);
+            }
+            condition[j] = base + block;
         }
     }
- 
-    /*0号进程从其余各进程中接收子矩阵a，得到经过变换的矩阵A*/
-    if (my_rank==0)
-    {
-        for(i=0;i<m;i++)
-            #pragma omp parallel for private(i,j,k) shared(A,a)
-            for(j=0;j<M;j++)
-                A(i*p,j)=a(i,j);
-    }
-    if (my_rank!=0)
-    {
-        for(i=0;i<m;i++)
-            MPI_Send(&a(i,0),M,MPI_FLOAT,0,i,MPI_COMM_WORLD);
-    }
-    else
-    {
-        for(i=1;i<p;i++)
-            for(j=0;j<m;j++)
-        {
-            MPI_Recv(&a(j,0),M,MPI_FLOAT,i,j,MPI_COMM_WORLD,&status);
-            #pragma omp parallel for private(i,j,k) shared(A,a,p)
-            for(k=0;k<M;k++)
-                A((j*p+i),k)=a(j,k);
-        }
-    }
-    if (my_rank==0)
-    {
-        memset(l,0,sizeof l);
-        memset(u,0,sizeof u);
-        for(i=0;i<M;i++)
-            l(i,i)=1.0;
-        for(i=0;i<M;i++)
-            for(j=0;j<M;j++)
-                if (i>j) l(i,j)=A(i,j);
-                else u(i,j)=A(i,j);
-        // printf("Input of file \"dataIn.txt\"\n");
-        // printf("%d\t %d\n",M, N);
-        // for(i=0;i<M;i++)
-        // {
-        //     for(j=0;j<N;j++)
-        //         printf("%f\t",A(i,j));
-        //     printf("\n");
-        // }
-        // printf("\nOutput of LU operation\n");
-        // printf("Matrix L:\n");
-        // for(i=0;i<M;i++)
-        // {
-        //     for(j=0;j<M;j++)
-        //         printf("%f\t",l(i,j));
-        //     printf("\n");
-        // }
-        // printf("Matrix U:\n");
-        // for(i=0;i<M;i++)
-        // {
-        //     for(j=0;j<M;j++)
-        //         printf("%f\t",u(i,j));
-        //     printf("\n");
-        // }
-    }
-    MPI_Finalize();
-    Environment_Finalize(a,f);
-
-    clock_t end = clock();
-    printf("thread %d:%lf(ms)\n",my_rank,1000.0*(end-begin)/CLOCKS_PER_SEC);
-    return(0);
+    end = omp_get_wtime();
+    printf("%.3f\n", (double)(end - start) * 1e3);
+    free(A);
+    free(Q);
+    free(condition);
+    return 0;
 }
